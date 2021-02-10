55ccc9718b085f96382a52e519e7826b1ec2bf85
2299 <zykovart@gmail.com>
2021-02-10 15:11:07 +0300

    create log/reg via firebase

--- src/main.js
+++ src/main.js
@@ -1,5 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import firebase from 'firebase'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCX0WyMb37g8caJwPp-27MpwBc0dyNDSec",
+  authDomain: "vue-gamepoisk.firebaseapp.com",
+  projectId: "vue-gamepoisk",
+  storageBucket: "vue-gamepoisk.appspot.com",
+  messagingSenderId: "885019530374",
+  appId: "1:885019530374:web:2a21f428c5b1e30959ad07"
+};
+
+firebase.initializeApp(firebaseConfig);
 
 createApp(App).use(router).mount('#app')
